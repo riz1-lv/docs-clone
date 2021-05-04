0fae540a00a3060044a05ff61520c8abb5e2c0e9
riz1-lv <riz.shkh1@gmail.com>
2021-05-04 17:06:50 -0400

    setup nodemon and typescript for server

--- web/src/components/TextEditor.tsx
+++ web/src/components/TextEditor.tsx
@@ -6,7 +6,7 @@ interface TextEditorProps {
 }
 const modules = {
   toolbar: [
-    [{ 'header': [1, 2,3,4,5,6,7, false] }],
+    [{ 'header': [1, 2,3,4,5,6, false] }],
     [{font:[]}],
     ['bold', 'italic', 'underline','strike', 'blockquote'],
     [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
