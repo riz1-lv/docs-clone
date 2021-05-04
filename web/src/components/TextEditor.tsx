d6dbcd2b8131e99add2ed019e30a2c4fd95bc6ce
riz1-lv <riz.shkh1@gmail.com>
2021-05-04 16:15:06 -0400

    added more toolbar options and initial server setup

--- web/src/components/TextEditor.tsx
+++ web/src/components/TextEditor.tsx
@@ -4,10 +4,23 @@ import ReactQuill from 'react-quill';
 interface TextEditorProps {
 
 }
+const modules = {
+  toolbar: [
+    [{ 'header': [1, 2,3,4,5,6,7, false] }],
+    [{font:[]}],
+    ['bold', 'italic', 'underline','strike', 'blockquote'],
+    [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+    ['link', 'image', "blockquote", "code-block"],
+    [{color:[]}, {background:[]}],
+    [{script:"sub"},{script:"super"}],
+    [{align: []}],
+    ['clean']
+  ],
+}
 //sdfsdfsdfsdfsfs
-export const TextEditor: React.FC<TextEditorProps> = ({}) => {
+export const TextEditor: React.FC<TextEditorProps> = () => {
     return (<>
-      <ReactQuill theme="snow" className="container" />
+      <ReactQuill theme="snow" className="container" modules={modules}/>
       
       </>
     );
