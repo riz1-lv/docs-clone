2d1efd097bb0c7427a39fbdbbdd4a0a971084b30
riz1-lv <riz.shkh1@gmail.com>
2021-05-04 18:48:04 -0400

    added socket.io server setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,21 @@
-console.log("hello")
+
+import {Server, Socket} from "socket.io";
+
+
+const io = new Server(3001,{
+  path: "/",
+  serveClient: false,
+  // below are engine.IO options
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false,
+  cors:{
+    "origin": "localhost:3000",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  }
+});
+
+
+io.on("connection",(data)=>{
+  console.log(data)
+})
